29c88943d727ab3475243f6a9ac6965e4009fab7
Amritha-k42 <amrithakpoojary262@gmail.com>
2025-06-20 17:23:48 +0530

    code file

---
+++ singlePort_BRAM_LUT/singleport_BRAM.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module singleport_BRAM(
+ input clk, we, [9:0] addr, [31:0] din, output [31:0] dout
+    );
+    reg [31:0]mem [0:1023];
+    
+    always @(posedge clk)
+     begin 
+     if(we) begin
+      mem[addr] <= din;
+      end
+      end
+     
+    assign dout = mem[addr]; 
+endmodule
